ab2e4b86c2b1f29281a5bdd8649fce76a3ebab65
Mehdi ABKHOUKH <mehdiab92@gmail.com>
2018-10-19 12:28:57 +0200

    test php

---
+++ src/AppBundle/Tests/AdminTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+final class AdminTest extends TestCase
+{
+    public function test1()
+    {
+        $this->assertTrue(
+            true
+        );
+    }
+
+}
