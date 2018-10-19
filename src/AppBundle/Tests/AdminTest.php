2f592ef76e1e9afdb40d665202bd88daff181df2
Mehdi ABKHOUKH <mehdiab92@gmail.com>
2018-10-19 12:34:47 +0200

    Final version phpunit ok

--- src/AppBundle/Tests/AdminTest.php
+++ src/AppBundle/Tests/AdminTest.php
@@ -4,11 +4,10 @@ use PHPUnit\Framework\TestCase;
 
 final class AdminTest extends TestCase
 {
-    public function test1()
+    public function testAdmin()
     {
         $this->assertTrue(
             true
         );
     }
-
 }
